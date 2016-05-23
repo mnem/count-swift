04c1f2f8803b1a0d8b515fdccbba8261c9786207
David Wagner <mnem@noiseandheat.com>
2016-05-23 23:05:35 +0100

    Quick hack to ignore games when calculating things

--- count-swift.rb
+++ count-swift.rb
@@ -8,6 +8,14 @@
 #
 # -- https://github.com/mnem
 
+# If you install the plist gem, the script
+# will exclude games from the result under
+# the assumption that games are unlikely
+# to switch to swift anytime soon.
+require "plist"
+# https://affiliate.itunes.apple.com/resources/documentation/genre-mapping/
+
+
 # Let's make a great big ugly assumption that the ipa files are in the standard place
 ITUNES_APP_ROOT = File.join(ENV['HOME'], 'Music/iTunes/iTunes Media/Mobile Applications', '*.ipa')
 
@@ -18,22 +26,33 @@ IPAS=Dir.glob ITUNES_APP_ROOT
 puts "Scaning #{IPAS.count} IPAs in #{ITUNES_APP_ROOT}"
 swift_libs = []
 swift_ipas = []
+non_swift_ipas = []
+ipa_count = 0
 IPAS.each do |ipa|
 	# Yep, we're still processing, look at the pretty dots!
-	print '.'
 
-	# List the contents of the ipa and search for some likely swift frameworks.
-	libs = `unzip -l \"#{ipa}\" | grep -i 'libswift.*\.dylib'`
-	if libs.length > 0
-		# Has some swift
-		swift_ipas << ipa
+	metadata = Plist::parse_xml `unzip -p "#{ipa}" iTunesMetadata.plist`
+	if metadata["genreId"] == 6014
+		print 'x'
+	else
+		print '.'
+		ipa_count = ipa_count + 1
 
-		# Store lib names in a hideously inefficient way
-		libs = libs.split "\n"
-		swift_libs << libs.map do |lib|
-			lib_parts = lib.split ' '
-			lib_parts.slice!(0..2)
-			File.basename lib_parts.join(' ') 
+		# List the contents of the ipa and search for some likely swift frameworks.
+		libs = `unzip -l \"#{ipa}\" | grep -i 'libswift.*\.dylib'`
+		if libs.length > 0
+			# Has some swift
+			swift_ipas << ipa
+
+			# Store lib names in a hideously inefficient way
+			libs = libs.split "\n"
+			swift_libs << libs.map do |lib|
+				lib_parts = lib.split ' '
+				lib_parts.slice!(0..2)
+				File.basename lib_parts.join(' ') 
+			end
+		else
+			non_swift_ipas << ipa
 		end
 	end
 end
@@ -41,6 +60,9 @@ end
 # Output some stats
 puts "\n\nUnique Swift libs found:"
 swift_libs.flatten.uniq.sort.each { |lib| puts "  #{lib}" }
+# puts "\nIPAs NOT containing Swift libs:"
+# non_swift_ipas.sort.each { |ipa| puts "  (No Swift) #{File.basename(ipa, '.ipa')}" }
 puts "\nIPAs containing Swift libs:"
 swift_ipas.sort.each { |ipa| puts "  #{File.basename(ipa, '.ipa')}" }
-puts "\nFrom #{IPAS.count} apps, #{swift_ipas.count} (#{(Float(swift_ipas.count)/IPAS.count*100.0).round}%) contain libswift*.dylib frameworks"
+# WARNING: Check for ipa_count being 0
+puts "\nFrom #{ipa_count} apps, #{swift_ipas.count} (#{(Float(swift_ipas.count)/ipa_count*100.0).round}%) contain libswift*.dylib frameworks"
